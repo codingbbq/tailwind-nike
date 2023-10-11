162911b33e6bbfae870aa3297bc803dee580fc76
codingbbq <codingbbq@gmail.com>
2023-10-11 18:23:51 +0530

    Footer section

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,7 +1,18 @@
+import { reviews } from "../constants";
+import ReviewCard from '../components/ReviewCard';
+
 const CustomerReviews = () => {
     return (
         <>
-            Customer Reviews
+            <section className="max-container ">
+                <h3 className="font-palanquin text-4xl text-center font-bold">What Our <span className="text-coral-red">Customers</span> say?</h3>
+                <p className="info-text m-auto mt-4 max-w-lg text-center">Here genuine stories from our satified customers about their exceptional experiences with us</p>
+                <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+                    {reviews.map((review) => (
+                        <ReviewCard key={review.customerName} imgURL={review.imgURL} customerName={review.customerName} rating={review.rating} feedback={review.feedback} />
+                    ))}
+                </div>
+            </section>
         </>
     )
 };
